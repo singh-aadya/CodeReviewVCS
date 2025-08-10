24edf5ae6a37864529268ca36b1a7536631e9cac
AADYA SINGH <152200033+singh-aadya@users.noreply.github.com>
2025-08-10 20:42:17 +0530

    Create EditOperation.java

---
+++ src/main/java/com/aadya/vcs/core/EditOperation.java
@@ -0,0 +1,41 @@
+package com.yourname.vcs.core;
+
+public class EditOperation {
+    public enum Type {
+        ADD, EDIT, DELETE
+    }
+
+    private final String fileName;
+    private final int lineNumber;
+    private final String oldLine;
+    private final String newLine;
+    private final Type operationType;
+
+    public EditOperation(String fileName, int lineNumber, String oldLine, String newLine, Type operationType) {
+        this.fileName = fileName;
+        this.lineNumber = lineNumber;
+        this.oldLine = oldLine;
+        this.newLine = newLine;
+        this.operationType = operationType;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getLineNumber() {
+        return lineNumber;
+    }
+
+    public String getOldLine() {
+        return oldLine;
+    }
+
+    public String getNewLine() {
+        return newLine;
+    }
+
+    public Type getOperationType() {
+        return operationType;
+    }
+}

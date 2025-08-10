ad3d4a135c5939d2e44395836a05dc7916756124
AADYA SINGH <152200033+singh-aadya@users.noreply.github.com>
2025-08-10 21:10:00 +0530

    Update ConsoleUI.java

--- src/main/java/com/aadya/vcs/ui/ConsoleUI.java
+++ src/main/java/com/aadya/vcs/ui/ConsoleUI.java
@@ -1,1 +1,26 @@
 
+package com.aadya.vcs.ui;
+
+import com.aadya.vcs.service.VersionControlSystem;
+
+import java.util.Scanner;
+
+public class ConsoleUI {
+    private VersionControlSystem vcs;
+    private Scanner scanner;
+
+    public ConsoleUI() {
+        vcs = new VersionControlSystem();
+        scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        System.out.println("=== Welcome to Code Review & Version Control Simulator ===");
+
+        while (true) {
+            printMenu();
+            System.out.print("Choice: ");
+            String input = scanner.nextLine().trim();
+
+            switch (input) {
+                case "1" -> createFile();

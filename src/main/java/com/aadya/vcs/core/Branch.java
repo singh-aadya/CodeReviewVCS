73856d8991ed7117a4398dbab3a0bcdbd2d42912
AADYA SINGH <152200033+singh-aadya@users.noreply.github.com>
2025-08-10 20:41:33 +0530

    Create Branch.java

---
+++ src/main/java/com/aadya/vcs/core/Branch.java
@@ -0,0 +1,23 @@
+package com.yourname.vcs.core;
+
+public class Branch {
+    private final String name;
+    private Commit head;
+
+    public Branch(String name, Commit head) {
+        this.name = name;
+        this.head = head;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Commit getHead() {
+        return head;
+    }
+
+    public void setHead(Commit newHead) {
+        this.head = newHead;
+    }
+}
